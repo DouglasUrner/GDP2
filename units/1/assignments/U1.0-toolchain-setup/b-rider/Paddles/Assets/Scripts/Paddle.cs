aacae909b7aecc6cf497e5a816e899e0287be22d
Douglas Urner <dlu@canishe.com>
2019-02-28 23:37:39 -0800

    Paddles working.

--- units/1/assignments/U1.0-toolchain-setup/b-rider/Paddles/Assets/Scripts/Paddle.cs
+++ units/1/assignments/U1.0-toolchain-setup/b-rider/Paddles/Assets/Scripts/Paddle.cs
@@ -1,18 +1,30 @@
-﻿using System.Collections;
+﻿/*
+ * These using statements are included by Unity when it generates the
+ * script file. For this game, they are not used. They are commented out
+ * so that they will not generate "advice" from Rider.
+ *
+using System.Collections;
 using System.Collections.Generic;
+ */
+
 using UnityEngine;
 
-public class Paddle : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Paddle : MonoBehaviour {
+	/*
+	 * Variables declared as public in the outer scope of a class which
+	 * derives from MonoBehavior will be visible in the Unity editor and
+	 * can be set there.
+	 */
+	public string Axis = " --- Select Input system axis for this paddle --- ";
+	public float Speed = 10;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Start is called before the first frame update
+	void Start() {
+	}
+
+	// Update is called once per frame
+	void Update() {
+		var v = Input.GetAxisRaw(Axis);
+		GetComponent<Rigidbody2D>().velocity = new Vector2(0, v) * Speed;
+	}
 }
