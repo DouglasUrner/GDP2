9e74b0082746ead92bf65c5f533bd893bd382e58
Douglas Urner <dlu@canishe.com>
2019-03-01 15:01:32 -0800

    Readability.

--- units/1/assignments/U1.0-toolchain-setup/b-rider/Paddles/Assets/Scripts/Paddle.cs
+++ units/1/assignments/U1.0-toolchain-setup/b-rider/Paddles/Assets/Scripts/Paddle.cs
@@ -9,22 +9,25 @@ using System.Collections.Generic;
 
 using UnityEngine;
 
-public class Paddle : MonoBehaviour {
-	/*
-	 * Variables declared as public in the outer scope of a class which
-	 * derives from MonoBehavior will be visible in the Unity editor and
-	 * can be set there.
-	 */
-	public string Axis = " --- Select Input system axis for this paddle --- ";
-	public float Speed = 10;
+public class Paddle : MonoBehaviour
+{
+    /*
+     * Variables declared as public in the outer scope of a class which
+     * derives from MonoBehavior will be visible in the Unity editor and
+     * can be set there.
+     */
+    public string Axis = " --- Select Input axis for this paddle --- ";
+    public float Speed = 10;
 
-	// Start is called before the first frame update
-	void Start() {
-	}
+// Start is called before the first frame update
+    private void Start()
+    {
+    }
 
-	// Update is called once per frame
-	void Update() {
-		var v = Input.GetAxisRaw(Axis);
-		GetComponent<Rigidbody2D>().velocity = new Vector2(0, v) * Speed;
-	}
+// Update is called once per frame
+    private void Update()
+    {
+        var v = Input.GetAxisRaw(Axis);
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, v) * Speed;
+    }
 }
