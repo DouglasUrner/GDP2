67533a835eb4a5555ba942504957a7b1be0e2f00
Douglas Urner <dlu@canishe.com>
2019-03-07 21:10:30 -0800

    Initial commit.

---
+++ units/1/assignments/U1.1-pong-in-unity/b-build/level-3/Quarles-Pong-2/Assets/Scripts/BallSpawnerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawnerController : MonoBehaviour
+{
+    public GameObject ballPrefab;
+
+    // Use this for initialization
+    void Start () {
+        GameObject ballClone = Instantiate(ballPrefab, this.transform.position, this.transform.rotation) as GameObject;
+        ballClone.transform.SetParent(this.transform);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if(transform.childCount == 0){
+            GameObject ballClone = Instantiate(ballPrefab, this.transform.position, this.transform.rotation) as GameObject;
+            ballClone.transform.SetParent(this.transform);
+        }
+    }
+}
