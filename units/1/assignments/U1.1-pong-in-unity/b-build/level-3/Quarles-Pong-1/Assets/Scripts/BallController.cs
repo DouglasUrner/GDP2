eccf8c096453ee5ba0278ab0089ebe9b3c477823
Douglas Urner <dlu@canishe.com>
2019-03-07 19:24:45 -0800

    Working ball.

---
+++ units/1/assignments/U1.1-pong-in-unity/b-build/level-3/Quarles-Pong-1/Assets/Scripts/BallController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+//speed of the ball
+    public float speed = 3.5F;
+
+    // The initial direction of the ball
+    private Vector2 spawnDir;
+
+    Rigidbody2D rb;
+    
+    void Start () {
+        // Get the ball's Rigidbody2D component.
+        rb = this.gameObject.GetComponent<Rigidbody2D>();
+
+        //generating random number based on possible initial directions
+        int rand = Random.Range(1,4);
+
+        //setting initial direction
+        if (rand == 1) {
+            spawnDir = new Vector2(1,1);
+        } else if (rand == 2) {
+            spawnDir = new Vector2(1,-1);
+        } else if (rand == 3) {
+            spawnDir = new Vector2(-1,-1);
+        } else if (rand == 4) {
+            spawnDir = new Vector2(-1,1);
+        }
+
+        //moving ball in initial direction and adding speed
+        rb.velocity = (spawnDir * speed);
+
+    }
+}
