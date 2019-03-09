f0d1e6d7e320d6389a5a68c10ce0154b368ee32a
Douglas Urner <dlu@canishe.com>
2019-03-08 23:34:28 -0800

    Scoring.

---
+++ units/1/assignments/U1.1-pong-in-unity/b-build/level-3/Quarles-Pong-2/Assets/Scripts/PointCounter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointCounter : MonoBehaviour
+{
+    public GameObject rightBound;
+    public GameObject leftBound;
+    Text text;
+    
+    // Use this for initialization
+    void Start () {
+        text = GetComponent<Text>();
+        text.text = rightBound.GetComponent<BoundController>().opponentScore + "\t\t" +
+                    leftBound.GetComponent<BoundController>().playerScore;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        text.text = rightBound.GetComponent<BoundController>().opponentScore + "\t\t" +
+                    leftBound.GetComponent<BoundController>().playerScore;
+    }
+}
