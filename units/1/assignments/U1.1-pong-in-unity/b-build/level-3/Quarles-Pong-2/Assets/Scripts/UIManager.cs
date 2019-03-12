5d8582fed07fdc9e3a9a6e05cfb64e63269e3cf7
Douglas Urner <dlu@canishe.com>
2019-03-11 17:02:21 -0700

    Cleaning up.

--- units/1/assignments/U1.1-pong-in-unity/b-build/level-3/Quarles-Pong-2/Assets/Scripts/UIManager.cs
+++ units/1/assignments/U1.1-pong-in-unity/b-build/level-3/Quarles-Pong-2/Assets/Scripts/UIManager.cs
@@ -3,83 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class UIManager : MonoBehaviour
-{
-    GameObject[] pauseObjects;
+public class UIManager : MonoBehaviour {
+  GameObject[] pauseObjects;
 
-    // Use this for initialization
-    void Start () {
-        pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
+  // Use this for initialization
+  void Start() {
+    pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
+  }
+
+  // Update is called once per frame
+  void Update() {
+    // Use the p button to pause and resume the game
+    if (Input.GetKeyDown(KeyCode.P)) {
+      if (Time.timeScale == 1) {
+        Time.timeScale = 0;
+        showPaused();
+      } else if (Time.timeScale == 0) {
+        Time.timeScale = 1;
+        hidePaused();
+      }
     }
+  }
 
-    // Update is called once per frame
-    void Update () {
+  // Reloads current Level
+  public void Reload() {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+  }
 
-        // Use the p button to pause and resume the game
-        if(Input.GetKeyDown(KeyCode.P))
-        {
-            if (Time.timeScale == 1)
-            {
-                Time.timeScale = 0;
-                showPaused();
-            } else if (Time.timeScale == 0){
-                Time.timeScale = 1;
-                hidePaused();
-            }
-        }
-
-
-        if (Time.timeScale == 0){
-            // Searches through pauseObjects for PauseText
-            foreach(GameObject g in pauseObjects){
-                if(g.name == "PauseText")
-                    //makes PauseText to Active
-                    g.SetActive(true);
-            }
-        } else {
-            // Searches through pauseObjects for PauseText
-            foreach(GameObject g in pauseObjects){
-                if(g.name == "PauseText")
-                    //makes PauseText to Inactive
-                    g.SetActive(false);
-            }
-        }
+  // Shows objects with ShowOnPause tag
+  public void showPaused() {
+    foreach (GameObject g in pauseObjects) {
+      g.SetActive(true);
     }
+  }
 
-
-    //Reloads the Level
-    public void Reload(){
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+  // Hides objects with ShowOnPause tag
+  public void hidePaused() {
+    foreach (GameObject g in pauseObjects) {
+      g.SetActive(false);
     }
+  }
 
-    //controls the pausing of the scene
-    public void pauseControl(){
-        if(Time.timeScale == 1)
-        {
-            Time.timeScale = 0;
-            showPaused();
-        } else if (Time.timeScale == 0){
-            Time.timeScale = 1;
-            hidePaused();
-        }
-    }
-
-    //shows objects with ShowOnPause tag
-    public void showPaused(){
-        foreach(GameObject g in pauseObjects){
-            g.SetActive(true);
-        }
-    }
-
-    //hides objects with ShowOnPause tag
-    public void hidePaused(){
-        foreach(GameObject g in pauseObjects){
-            g.SetActive(false);
-        }
-    }
-
-    //loads inputted level
-    public void LoadLevel(string scene){
-        SceneManager.LoadScene(scene);
-    }
+  // Loads inputted level
+  public void LoadLevel(string scene) {
+    SceneManager.LoadScene(scene);
+  }
 }
