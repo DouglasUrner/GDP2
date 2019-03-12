63fd4cc61d274d4dccd42cbbdd5e77393cfde51e
Douglas Urner <dlu@canishe.com>
2019-03-11 17:37:39 -0700

    Working on game state.

--- units/1/assignments/U1.1-pong-in-unity/b-build/level-3/Quarles-Pong-2/Assets/Scripts/UIManager.cs
+++ units/1/assignments/U1.1-pong-in-unity/b-build/level-3/Quarles-Pong-2/Assets/Scripts/UIManager.cs
@@ -4,30 +4,56 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour {
-  GameObject[] pauseObjects;
+  
+  public int winningScore = 7;
+  
+  // References to bound objects.
+  public BoundController leftBound, rightBound;
+  
+  // Game state.
+  private bool gameOver, leftWon, rightWon;
+  
+  // Objects to display on pause and win.
+  GameObject[] pauseObjects, winObjects;
 
   // Use this for initialization
   void Start() {
     pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
+    winObjects = GameObject.FindGameObjectsWithTag("ShowOnWin");
+    hideWin();
   }
 
   // Update is called once per frame
   void Update() {
     // Use the p button to pause and resume the game
     if (Input.GetKeyDown(KeyCode.P)) {
-      if (Time.timeScale == 1) {
-        Time.timeScale = 0;
-        showPaused();
-      } else if (Time.timeScale == 0) {
-        Time.timeScale = 1;
-        hidePaused();
-      }
+        pauseControl();
+    }
+
+    if (rightBound.opponentScore >= winningScore) {
+      gameOver = true;
+    } else if (leftBound.playerScore >= winningScore) {
+      gameOver = true;
+    }
+
+    if (gameOver) {
+      showWin();
     }
   }
 
   // Reloads current Level
   public void Reload() {
     SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+  }
+  
+  public void pauseControl() {
+    if (Time.timeScale == 1) {
+      Time.timeScale = 0;
+      showPaused();
+    } else if (Time.timeScale == 0) {
+      Time.timeScale = 1;
+      hidePaused();
+    }
   }
 
   // Shows objects with ShowOnPause tag
@@ -40,6 +66,18 @@ public class UIManager : MonoBehaviour {
   // Hides objects with ShowOnPause tag
   public void hidePaused() {
     foreach (GameObject g in pauseObjects) {
+      g.SetActive(false);
+    }
+  }
+
+  private void showWin() {
+    foreach (var g in winObjects) {
+      g.SetActive(true);
+    }
+  }
+
+  private void hideWin() {
+    foreach (var g in winObjects) {
       g.SetActive(false);
     }
   }
