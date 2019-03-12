c6ae53515795e3c15fcc87e57689240bfe15a3f4
Douglas Urner <dlu@canishe.com>
2019-03-11 21:43:39 -0700

    Comment out redundant code.

--- units/1/assignments/U1.1-pong-in-unity/b-build/level-3/Quarles-Pong-2/Assets/Scripts/BallSpawnerController.cs
+++ units/1/assignments/U1.1-pong-in-unity/b-build/level-3/Quarles-Pong-2/Assets/Scripts/BallSpawnerController.cs
@@ -8,8 +8,12 @@ public class BallSpawnerController : MonoBehaviour
 
     // Use this for initialization
     void Start () {
+        /*
+         * This code seems redundant - Update() will do the same thing.
+         * 
         GameObject ballClone = Instantiate(ballPrefab, this.transform.position, this.transform.rotation) as GameObject;
         ballClone.transform.SetParent(this.transform);
+         */
     }
 
     // Update is called once per frame
